8a43f9461fe3abe88825fbbee98b0f4bf7432805
LFCRab <56073739+LFCRab@users.noreply.github.com>
2020-09-11 14:06:22 +0100

    :sparkles: enemy AI added in :sparkles:

---
+++ using-unity-with-git/Assets/Scripts/EnemyBullet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour
+{
+    public GameObject hitEffect;
+    public AudioSource hitAudio;
+
+    void start()
+    {
+        hitAudio = GetComponent<AudioSource>();
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag != "Enemy")
+        {
+            hitAudio.Play();
+            Destroy(collision.gameObject, 0.2f);
+        }
+        if (collision.gameObject.tag == "Player")
+        {
+            GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+            Destroy(effect, 0.4f);
+        }
+    }
+}
